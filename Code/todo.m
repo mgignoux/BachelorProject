f7aebacbc61742f2daedcc1d523abb5e4b720421
mgignoux <madeleine.gignoux@gmail.com>
2023-03-19 19:05:36 +0100

    Update todo

--- Code/todo.m
+++ Code/todo.m
@@ -2,6 +2,10 @@
 
 %{ 
 - complexity analysis
-- think about analysis and testing
+- think about creating a combined function
 - add error cases
+- implement all the inverses
+- clean up helpers
+- check everything still runs post changes
+- think about how to test
 %}
