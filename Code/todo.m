687b670d5652e55faf6d8b1dc35eee0581c74994
mgignoux <madeleine.gignoux@gmail.com>
2023-05-05 13:58:09 +0200

    Update todo

--- Code/todo.m
+++ Code/todo.m
@@ -4,8 +4,5 @@
 - complexity analysis
 - think about creating a combined function
 - add error cases
-- implement all the inverses
-- clean up helpers
-- check everything still runs post changes
-- think about how to test
+- update radix-N with new knowledge
 %}
