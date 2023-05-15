ed90a50e4a257bc7607f98bb98b06caff98062de
mgignoux <madeleine.gignoux@gmail.com>
2023-05-15 14:55:43 +0200

    Add readme

--- Code/dihedralDFT/dihedralDFT.m
+++ Code/dihedralDFT/dihedralDFT.m
@@ -2,7 +2,7 @@ function fh = dihedralDFT(f)
     L = length(f);
     fh = zeros(2,2,L);
 
-    for n = 0:L-1
+    for n = 1:L/2-1
         for k1 = 0:1
             for k2 = 0:L-1
                 fh(:,:,n+1) = fh(:,:,n+1) + f(k1+1,k2+1).*(conj(dihedralRep(k1,k2,n,L)));
--- Code/dihedralDFT/dihedralIDFT.m
+++ Code/dihedralDFT/dihedralIDFT.m
@@ -4,7 +4,7 @@ function f = dihedralIDFT(fh)
 
     for k1 = 0:1
         for k2 = 0:L-1
-            for n = 0:L-1
+            for n = 1:L/2-1
                 f(k1+1,k2+1) = f(k1+1,k2+1) + sum((fh(:,:,n+1).*dihedralRep(k1,k2,n,L)),"all");
             end
         end
